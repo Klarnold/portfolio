e95cc0e2dd4fc687259052f42ad6c6b5c63aeff3
Lev <for.inst.techies@mail.ru>
2025-09-28 19:47:28 +0300

    packet managing
    deleteng some stuff

--- PC/3dTopdown/M03.gobot_the_sorcerer_workbook/lessons/game.gd
+++
@@ -1,9 +0,0 @@
-class_name Game extends Node3D
-
-
-@onready var _directional_light_3d: DirectionalLight3D = %DirectionalLight3D
-
-
-#
-#func _physics_process(delta: float) -> void:
-	#_directional_light_3d.rotate_z(delta * 0.5)
